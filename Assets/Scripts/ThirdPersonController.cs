29cda851d0807a02149fd33ea293753fa09fc405
Aidan Pilato <aidan@red-bean.com>
2026-02-02 15:04:59 -0500

    Basic Cinemachine vcam implementation

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -136,7 +136,7 @@ namespace StarterAssets
 
         private void LateUpdate()
         {
-            CameraRotation();
+            //CameraRotation();
         }
 
         private void AssignAnimationIDs()
